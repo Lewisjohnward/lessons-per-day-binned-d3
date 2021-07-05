c02b05e5262af10e041f7b0afa447f512f1b1d35
Lewis Ward <lewisjohnward+git1@gmail.com>
2021-07-05 19:00:26 +0100

    working graph

---
+++ src/useData.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from "react"
+import {csv} from "d3"
+
+const csvUrl = "https://gist.githubusercontent.com/Lewisjohnward/7a8eb5a17366a2cc83e1c414e06c492b/raw/694a4554ea8f06007acb8c5419a4b3f33e94fc23/Lessons.csv"
+
+export const useData = () => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        const row = d => {
+            d.date = new Date(+d.date)
+            d.length = +d.length
+            d.id = +d.id
+            return d
+        }
+
+        csv(csvUrl, row).then(setData)
+
+
+    }, [])
+
+    return data
+}
